abbd6e2ac7171bbd77f1f6037f10ec7fba2586cb
rsankar22 <rsankar23@utexas.edu>
2021-08-04 18:44:59 -0500

    Full method - valid

--- validationSchema.py
+++ validationSchema.py
@@ -7,7 +7,7 @@ from openpyxl import load_workbook
 def excel_valid(file):
     workbook = load_workbook(file)
     return workbook
-    # sheet = workbook.get_sheet_by_name('Sheet1')
-    # for i in sheet:
-    #     for j in sheet[i]:
-    #         return  (sheet.cell(row=i,column=j).value)
+    sheet = workbook.get_sheet_by_name('Sheet1')
+    for i in sheet:
+        for j in sheet[i]:
+            return  (sheet.cell(row=i,column=j).value)
